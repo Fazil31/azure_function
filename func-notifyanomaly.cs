00eec9588b9b70ea9c4968fa4dbf8195c101b13d
Fazil Hassan <fazil.hassan@accesshq.com>
2023-02-23 17:40:17 +1030

    update function to include logic app

--- func-notifyanomaly.cs
+++ func-notifyanomaly.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 using Azure.Messaging.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using System.Net.Http;
 
 namespace Company.Function
 {
     public static class func_notifyanomaly
     {
+        private static string logicAppUri = @"https://prod-07.australiaeast.logic.azure.com:443/workflows/99250cea29094b0d9197ed1d0cf92ccc/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LiTb95Y7gKzIvCY8PXgz-2jgaSf0kHeUqUqvknfwL68";
+        private static HttpClient httpClient = new HttpClient();
+
         [FunctionName("func_notifyanomaly")]
         public static async Task Run([EventHubTrigger("eh1312", Connection = "eventhub1312_RootManageSharedAccessKey_EVENTHUB")] EventData[] events, ILogger log)
         {
@@ -20,8 +24,13 @@ namespace Company.Function
             {
                 try
                 {
+                    string messageBody = eventData.EventBody.ToString();
                     // Replace these two lines with your processing logic.
-                    log.LogInformation($"C# Event Hub trigger function processed a message: {eventData.EventBody}");
+                    log.LogInformation($"C# Event Hub trigger function processed a message: {messageBody}");
+
+                    var response = await httpClient.PostAsync(logicAppUri, new StringContent(messageBody, Encoding.UTF8, "application/json"));
+                    log.LogInformation($"C# IoT Hub trigger function processed another message: {messageBody}");
+
                     await Task.Yield();
                 }
                 catch (Exception e)
